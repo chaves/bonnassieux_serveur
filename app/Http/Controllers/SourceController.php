e502e4c7409e17247a7888f6751d02cedfefbc64
Bruno Chaves <chabruno@gmail.com>
2020-02-28 09:39:15 +0100

    paginate 200

--- app/Http/Controllers/SourceController.php
+++ app/Http/Controllers/SourceController.php
@@ -9,12 +9,12 @@ class SourceController extends Controller
 {
     public function index()
     {
-        return Source::with('cities', 'domains', 'persons', 'groups', 'matters')->where('validated', 0)->orderBy('date', 'asc')->paginate(1000);
+        return Source::with('cities', 'domains', 'persons', 'groups', 'matters')->where('validated', 0)->orderBy('date', 'asc')->paginate(200);
     }
 
     public function validated(Request $request)
     {
-        return Source::with('cities', 'domains', 'persons', 'groups', 'matters')->where('validated', 1)->orderBy('date', 'asc')->paginate(1000);
+        return Source::with('cities', 'domains', 'persons', 'groups', 'matters')->where('validated', 1)->orderBy('date', 'asc')->paginate(200);
     }
 
     public function update(Request $request)
