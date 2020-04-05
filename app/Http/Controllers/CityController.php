805293da5430cbfc8c7beb81de2f6e751c92d2bd
Bruno Chaves <chabruno@gmail.com>
2020-04-05 13:28:59 +0200

    data for map

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\City;
+use Illuminate\Support\Facades\DB;
 
 class CityController extends Controller
 {
@@ -44,5 +45,19 @@ class CityController extends Controller
         $city->sources()->detach();
         $city->delete();
     }
+
+    public function getForMap(){
+        $data = DB::table('city_source')
+                    ->select(DB::raw('YEAR(date) as year, city_source.city_id as city_id, nom, count(*) as total, longitude, latitude'))
+                    ->join('sources', 'city_source.source_id','=','sources.id')
+                    ->join('coordinates', 'coordinates.city_id','=','city_source.city_id')
+                    ->whereNotNull('nom')
+                    ->groupBy('city_source.city_id', 'year')
+                    ->orderByRaw('year ASC, total ASC')
+                    ->get();
+        return $data;
+    }
+
+
 
 }
